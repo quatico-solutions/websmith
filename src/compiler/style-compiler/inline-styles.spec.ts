4d0b117b6179167d8aed564da764010b5792a809
Jan Wloka <jan.wloka@quatico.com>
2021-03-06 10:26:07 +0100

    c Adds missing license

--- src/compiler/style-compiler/inline-styles.spec.ts
+++ src/compiler/style-compiler/inline-styles.spec.ts
@@ -1,3 +1,17 @@
+/*
+ * @license
+ *
+ * Copyright (c) 2017-2021 Quatico Solutions AG
+ * Förrlibuckstrasse 220, 8005 Zurich, Switzerland
+ *
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Quatico Solutions AG, ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Quatico.
+ */
 // tslint:disable: object-literal-sort-keys
 import * as ts from "typescript";
 import { createBrowserSystem, getSourceFile } from "../../environment";
