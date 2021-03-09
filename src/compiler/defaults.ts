2e0399e032f317bb86c3c634cb5413b36e6a5e3a
Jan Wloka <jan.wloka@quatico.com>
2021-03-09 11:58:21 +0100

    E Reenable tsLibDefautls

--- src/compiler/defaults.ts
+++ src/compiler/defaults.ts
@@ -12,6 +12,7 @@
  * accordance with the terms of the license agreement you entered into
  * with Quatico.
  */
+// tslint:disable: object-literal-sort-keys
 import * as ts from "typescript";
 
 export const tsDefaults: ts.CompilerOptions = {
@@ -48,18 +49,16 @@ export const tsDefaults: ts.CompilerOptions = {
  * time using the Webpack raw-loader.
  */
 export const tsLibDefaults: { [name: string]: string } = {
-    // FIXME: raw-loader imports cannot be found with current tsconfig.json
-    // "/dom.d.ts": require("!!raw-loader!typescript/lib/lib.dom.d.ts"),
-    // "/es2015.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.d.ts"),
-    // "/lib.es2015.collection.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.collection.d.ts"),
-    // "/lib.es2015.core.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.core.d.ts"),
-    // "/lib.es2015.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.d.ts"),
-    // "/lib.es2015.generator.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.generator.d.ts"),
-    // "/lib.es2015.iterable.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.iterable.d.ts"),
-    // "/lib.es2015.promise.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.promise.d.ts"),
-    // "/lib.es2015.proxy.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.proxy.d.ts"),
-    // "/lib.es2015.reflect.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.reflect.d.ts"),
-    // "/lib.es2015.symbol.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.symbol.d.ts"),
-    // "/lib.es2015.symbol.wellknown.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.symbol.wellknown.d.ts"),
-    // "/lib.es5.d.ts": require("!!raw-loader!typescript/lib/lib.es5.d.ts"),
+    "/lib.es2015.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.d.ts"),
+    "/lib.es2015.collection.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.collection.d.ts"),
+    "/lib.es2015.core.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.core.d.ts"),
+    "/lib.es2015.generator.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.generator.d.ts"),
+    "/lib.es2015.iterable.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.iterable.d.ts"),
+    "/lib.es2015.promise.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.promise.d.ts"),
+    "/lib.es2015.proxy.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.proxy.d.ts"),
+    "/lib.es2015.reflect.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.reflect.d.ts"),
+    "/lib.es2015.symbol.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.symbol.d.ts"),
+    "/lib.es2015.symbol.wellknown.d.ts": require("!!raw-loader!typescript/lib/lib.es2015.symbol.wellknown.d.ts"),
+    "/lib.es5.d.ts": require("!!raw-loader!typescript/lib/lib.es5.d.ts"),
+    "/lib.dom.d.ts": require("!!raw-loader!typescript/lib/lib.dom.d.ts"),
 };
