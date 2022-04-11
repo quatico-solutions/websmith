b3bec42001d499a2f4e412f10c208a53ddc623c9
Jan Wloka <jan.wloka@quatico.com>
2022-04-11 07:55:51 +0200

    d Adds JS Doc for TargetConfig and ResultProcessor

--- packages/api/src/ResultProcessor.ts
+++ packages/api/src/ResultProcessor.ts
@@ -12,4 +12,15 @@
  * accordance with the terms of the license agreement you entered into
  * with Quatico.
  */
+/**
+ * ResultProcessors consume all output files from the compilation process
+ * and can create additional files. They cannot alter the output files,
+ * but have access to the transformed source code and compilation results.
+ * ResultProcessors are executed after all other Addon types and the actual
+ * compilation is completed. All ResultProcessors are executed in order
+ * of their registration once for every target.
+ *
+ * Use this result processor function to create additional output, such as
+ * other compilation results or documentation based on your output.
+ */
 export type ResultProcessor = (fileNames: string[]) => void;
--- packages/api/src/TargetConfig.ts
+++ packages/api/src/TargetConfig.ts
@@ -15,7 +15,12 @@
 import ts from "typescript";
 
 /**
- *
+ * This type represents the configuration options for a target. It is used in
+ * the `targets` section of the `websmith.config.json` file. You can specify
+ *  - the list of addons to apply for this target
+ *  - whether output files should be written to disk
+ *  - specific configuration properties used by your addon
+ *  - a set of compiler options for the TypeScript compiler for this target
  */
 export type TargetConfig = {
     addons?: string[];
