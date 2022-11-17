bc059dd742f66d61c09d66d351219cb15bc23290
Marc Schärer <marc.schaerer@quatico.com>
2022-11-17 14:41:46 +0100

    b Fix imports

--- packages/api/src/AddonActivator.ts
+++ packages/api/src/AddonActivator.ts
@@ -4,7 +4,7 @@
  *   Licensed under the MIT License. See LICENSE in the project root for license information.
  * ---------------------------------------------------------------------------------------------
  */
-import { AddonContext } from "./";
+import { AddonContext } from "./AddonContext";
 
 export interface AddonActivator {
     (ctx: AddonContext): void;
