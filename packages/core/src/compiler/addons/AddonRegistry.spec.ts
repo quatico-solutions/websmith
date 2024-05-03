693bbc08ab891513b87399f34ab3de39f163d99c
Jan Wloka <jan.wloka@quatico.com>
2024-05-03 10:20:37 +0200

    t Provides additional unit tests for AddonRegistry

--- packages/core/src/compiler/addons/AddonRegistry.spec.ts
+++ packages/core/src/compiler/addons/AddonRegistry.spec.ts
@@ -54,91 +54,42 @@ describe("getAddons", () => {
     it("returns empty addons w/ empty addons directory", () => {
         const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
 
-        expect(testObj.getAvailableAddons()).toEqual([]);
+        expect(testObj.getAvailableAddons()).toHaveLength(0);
     });
 
     it("returns addons w/ single addon in addon directory", () => {
-        system.writeFile("./addons/expected/addon.js", "export const activate = () => {};");
-        jest.mock(
-            "/addons/expected/addon",
-            () => {
-                return { activate: jest.fn() };
-            },
-            { virtual: true }
-        );
+        createAddon("addons/expected/addon");
 
         const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
 
-        expect(testObj.getAvailableAddons().map(it => it.getName())).toEqual(["expected"]);
+        expect(testObj.getAvailableAddons().getNames()).toEqual(["expected"]);
     });
 
     it("returns addons w/ multiple addons in addon directory", () => {
-        system.writeFile("./addons/one/addon.js", "export const activate = () => {};");
-        jest.mock(
-            "/addons/one/addon",
-            () => {
-                return { activate: jest.fn() };
-            },
-            { virtual: true }
-        );
-        system.writeFile("./addons/two/addon.js", "export const activate = () => {};");
-        jest.mock(
-            "/addons/two/addon",
-            () => {
-                return { activate: jest.fn() };
-            },
-            { virtual: true }
-        );
-        system.writeFile("./addons/three/addon.js", "export const activate = () => {};");
-        jest.mock(
-            "/addons/three/addon",
-            () => {
-                return { activate: jest.fn() };
-            },
-            { virtual: true }
-        );
+        createAddon("addons/one/addon");
+        createAddon("addons/two/addon");
+        createAddon("addons/three/addon");
 
         const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
 
-        expect(testObj.getAvailableAddons().map(it => it.getName())).toEqual(["one", "two", "three"]);
+        expect(testObj.getAvailableAddons().getNames()).toEqual(["one", "two", "three"]);
     });
 
     it("returns valid addons w/ invalid and valid addons in addon directory", () => {
-        system.writeFile("./addons/expected/addon.js", "export const activate = () => {};");
-        jest.mock(
-            "/addons/expected/addon",
-            () => {
-                return { activate: jest.fn() };
-            },
-            { virtual: true }
-        );
-        system.writeFile("./addons/invalid/addon.js", "export const whatever = () => {};");
-        jest.mock(
-            "/addons/invalid/addon",
-            () => {
-                return { whatever: jest.fn() };
-            },
-            { virtual: true }
-        );
+        createAddon("addons/expected/addon");
+        createAddon("addons/invalid/addon", "export const whatever = () => {};", { whatever: jest.fn() });
 
         const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
 
-        expect(testObj.getAvailableAddons().map(it => it.getName())).toEqual(["expected"]);
+        expect(testObj.getAvailableAddons().getNames()).toEqual(["expected"]);
     });
 
     it("returns no addons w/ empty files in addon directory", () => {
-        system.writeFile("./addons/empty/addon.js", "");
-        jest.mock(
-            "/addons/empty/addon",
-            () => {
-                return {};
-            },
-            { virtual: true }
-        );
+        createAddon("addons/empty/addon", "", {});
 
         const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
 
-        expect(testObj.getAvailableAddons()).toEqual([]);
+        expect(testObj.getAvailableAddons()).toHaveLength(0);
     });
 
     it("reports warning w/ non-existing addons name", () => {
@@ -169,3 +120,175 @@ describe("getAddons", () => {
         expect(reporter.reportDiagnostic).toHaveBeenCalledWith(new WarnMessage('Missing addons for target "target": "does-not-exist".'));
     });
 });
+
+describe("getAddonsDir", () => {
+    it("returns addons directory with addonsDir config", () => {
+        const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
+
+        expect(testObj.getAddonsDir()).toEqual("./addons");
+    });
+
+    it("returns undefined w/o addons directory", () => {
+        const testObj = new AddonRegistry({ reporter, system } as any);
+
+        expect(testObj.getAddonsDir()).toBeUndefined();
+    });
+});
+
+describe("refresh", () => {
+    it("refreshes addons w/ new addons", () => {
+        createAddon("addons/expected/addon");
+
+        const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
+
+        expect(testObj.getAvailableAddons().getNames()).toEqual(["expected"]);
+
+        createAddon("addons/new/addon");
+
+        testObj.refresh();
+
+        expect(testObj.getAvailableAddons().getNames()).toEqual(["expected", "new"]);
+    });
+
+    it("refreshes addons w/o new addons", () => {
+        createAddon("addons/expected/addon");
+
+        const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
+
+        expect(testObj.getAvailableAddons().getNames()).toEqual(["expected"]);
+
+        testObj.refresh();
+
+        expect(testObj.getAvailableAddons().getNames()).toEqual(["expected"]);
+    });
+
+    it("refreshes addons w/ removed addons", () => {
+        createAddon("addons/expected/addon");
+        createAddon("addons/removed/addon");
+
+        const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
+
+        expect(testObj.getAvailableAddons().getNames()).toEqual(["expected", "removed"]);
+
+        removeAddon("addons/removed/addon");
+
+        testObj.refresh();
+
+        expect(testObj.getAvailableAddons().getNames()).toEqual(["expected"]);
+    });
+
+    it("refreshes addons w/ empty addons directory", () => {
+        createAddon("addons/expected/addon");
+
+        const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
+
+        expect(testObj.getAvailableAddons().getNames()).toEqual(["expected"]);
+
+        removeAddon("addons/expected/addon");
+
+        testObj.refresh();
+
+        expect(testObj.getAvailableAddons()).toHaveLength(0);
+    });
+});
+
+describe("reportMissingAddons", () => {
+    it("reports missing addons directory", () => {
+        reporter.reportDiagnostic = jest.fn();
+
+        const testObj = new AddonRegistry({ addonsDir: "./expected", reporter, system });
+
+        // @ts-expect-error private property access
+        testObj.reportMissingAddons(undefined, []);
+
+        expect(reporter.reportDiagnostic).toHaveBeenCalledWith(new WarnMessage('Addons directory "./expected" does not exist.'));
+    });
+
+    it("reports missing addons w/ target", () => {
+        system.createDirectory("./addons");
+        reporter.reportDiagnostic = jest.fn();
+
+        const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
+
+        // @ts-expect-error private property access
+        testObj.reportMissingAddons("my-target", ["missing"]);
+
+        expect(reporter.reportDiagnostic).toHaveBeenCalledWith(new WarnMessage('Missing addons for target "my-target": "missing".'));
+    });
+
+    it("reports missing addons w/o target", () => {
+        system.createDirectory("./addons");
+        reporter.reportDiagnostic = jest.fn();
+
+        const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
+
+        // @ts-expect-error private property access
+        testObj.reportMissingAddons(undefined, ["missing"]);
+
+        expect(reporter.reportDiagnostic).toHaveBeenCalledWith(new WarnMessage('Missing addons: "missing".'));
+    });
+
+    it("does not report missing addons w/o missing addons", () => {
+        system.createDirectory("./addons");
+        reporter.reportDiagnostic = jest.fn();
+
+        const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
+
+        // @ts-expect-error private property access
+        testObj.reportMissingAddons(undefined, []);
+
+        expect(reporter.reportDiagnostic).not.toHaveBeenCalled();
+    });
+});
+
+describe("findAddons", () => {
+    it("finds addons w/ single addon in addons directory", () => {
+        createAddon("addons/expected/addon");
+
+        const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
+
+        expect(testObj.getAvailableAddons().getNames()).toEqual(["expected"]);
+    });
+
+    it("finds addons w/ multiple addons in addons directory", () => {
+        createAddon("addons/one/addon");
+        createAddon("addons/two/addon");
+        createAddon("addons/three/addon");
+
+        const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
+
+        expect(testObj.getAvailableAddons().getNames()).toEqual(["one", "two", "three"]);
+    });
+
+    it("finds valid addons w/ invalid and valid addons in addons directory", () => {
+        createAddon("addons/expected/addon");
+        createAddon("addons/invalid/addon", "export const whatever = () => {};", { whatever: jest.fn() });
+
+        const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
+
+        expect(testObj.getAvailableAddons().getNames()).toEqual(["expected"]);
+    });
+
+    it("finds no addons w/ empty files in addons directory", () => {
+        createAddon("addons/empty/addon", "", {});
+
+        const testObj = new AddonRegistry({ addonsDir: "./addons", reporter, system });
+
+        expect(testObj.getAvailableAddons()).toHaveLength(0);
+    });
+});
+
+const createAddon = (path: string, code = "export const activate = () => {};", mock: object = { activate: jest.fn() }) => {
+    system.writeFile(`./${path}.js`, code);
+    jest.mock(
+        `/${path}`,
+        () => {
+            return mock;
+        },
+        { virtual: true }
+    );
+};
+
+const removeAddon = (path: string) => {
+    system.deleteFile!(`./${path}.js`);
+};
