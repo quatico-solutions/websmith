8f16be2b998964f6e86d30db4f3387fef63875f4
Jan Wloka <jan.wloka@quatico.com>
2024-05-09 13:37:58 +0200

    c Adds missing license headers

--- packages/webpack/src/loader.ts
+++ packages/webpack/src/loader.ts
@@ -1,3 +1,9 @@
+/*
+ * ---------------------------------------------------------------------------------------------
+ *   Copyright (c) Quatico Solutions AG. All rights reserved.
+ *   Licensed under the MIT License. See LICENSE in the project root for license information.
+ * ---------------------------------------------------------------------------------------------
+ */
 import { LoaderContext } from "webpack";
 import { initializeInstance, setInstanceInCache } from "./instance-cache";
 import { getLoaderOptions, PluginOptions } from "./loader-options";
