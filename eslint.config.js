622dd450a4f470c7e898a6de9a3e963440e79301
Jan Wloka <jan.wloka@quatico.com>
2024-05-09 13:37:11 +0200

    a Auomatic liniting fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-var-requires */
 /*
  * ---------------------------------------------------------------------------------------------
  *   Copyright (c) Quatico Solutions AG. All rights reserved.
