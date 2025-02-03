187378a2e116fd14d16b54e7afdd82f85f5f5e8a
Jan Wloka <jan.wloka@quatico.com>
2025-02-03 10:24:08 +0100

    E Upgrades deprecated dependencies and replaces crypto with crypto-js
    Upgrade jest configs and move test-runner and integration tests from websmith-sandbox #56

--- packages/webpack/tests/webpack.test.ts
+++ packages/webpack/tests/webpack.test.ts
@@ -1,5 +1,5 @@
 import { type CompilerOptions as WebsmithOptions } from "@quatico/websmith-core";
-import { webpack } from "../../node/src";
+import { webpack } from "@quatico/websmith-node";
 import fs from "fs";
 import path from "path";
 import ts from "typescript";
