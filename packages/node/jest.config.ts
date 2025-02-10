2f2bb2b87d36607b9afa29906deec2e98a6a1589
Jan Wloka <jan.wloka@quatico.com>
2025-02-10 21:32:45 +0100

    t Increases testTimeout to 60s for websmith-node tests to pass GH pipeline
    Upgrade jest configs and move test-runner and integration tests from websmith-sandbox #56

--- packages/node/jest.config.ts
+++ packages/node/jest.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
     },
     setupFilesAfterEnv: ["./test/jest.setup.ts"],
     testRegex: "(tests|src)/.+\\.spec\\.ts$",
-    testTimeout: 25000,
+    testTimeout: 60000,
     transform: {
         "^.+\\.(js|ts)$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.test.json", diagnostics: false, isolatedModules: true }],
     },
