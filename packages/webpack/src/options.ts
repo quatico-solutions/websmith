ebb59973b9d7f86d6070ac4a82f3b271809810a6
Jan Wloka <jan.wloka@quatico.com>
2025-02-13 14:25:05 +0100

    R Removes the buildDir config property from the websmith-loader options
    Introducing Profiles: Replace compilation targets with profiles #55

--- packages/webpack/src/options.ts
+++ packages/webpack/src/options.ts
@@ -18,14 +18,14 @@ import ts from "typescript";
 import { type WebsmithLoaderConfig } from "./WebsmithLoaderConfig";
 
 export const createOptions = (args: WebsmithLoaderConfig, reporter: Reporter = new NoReporter(), system = ts.sys): CompilerOptions => {
-    const { buildDir, config, configFile, debug = false, project = "./tsconfig.json", targets, tsConfig, transpileOnly } = args;
+    const { config, configFile, debug = false, project = "./tsconfig.json", targets, tsConfig, transpileOnly } = args;
 
     const cliArgs = resolveTsConfig(project, system);
     cliArgs.options = { ...cliArgs.options, ...tsConfig };
     const compilationConfig = configFile ? resolveCompilationConfig(configFile, reporter, system) : undefined;
 
     const projectDirectory = (configFile && dirname(configFile)) ?? (cliArgs.raw?.configFilePath && dirname(cliArgs.raw?.configFilePath));
-    cliArgs.options.outDir = system.resolvePath(cliArgs.options.outDir ?? buildDir ?? "./lib");
+    cliArgs.options.outDir = system.resolvePath(cliArgs.options.outDir ?? "./lib");
     if (projectDirectory) {
         cliArgs.options = updateCompilerOptions(cliArgs.options, system, projectDirectory);
     }
@@ -44,7 +44,7 @@ export const createOptions = (args: WebsmithLoaderConfig, reporter: Reporter = n
     }
 
     return {
-        buildDir: buildDir ?? system.getCurrentDirectory(),
+        buildDir: system.getCurrentDirectory(),
         cliArgs,
         ...(mergedConfig && { config: mergedConfig }),
         ...(configFile && { configFile }),
