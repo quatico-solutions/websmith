c58881be6049ff5682a2a5da638e00e0ae3cc425
Jan Wloka <jan.wloka@quatico.com>
2025-02-14 17:26:36 +0100

    F depends property in CompilationProfile and validation
    Introducing Profiles: Replace compilation targets with profiles #55

--- packages/core/src/compiler/config/resolve-compiler-config.spec.ts
+++ packages/core/src/compiler/config/resolve-compiler-config.spec.ts
@@ -4,6 +4,7 @@
  *   Licensed under the MIT License. See LICENSE in the project root for license information.
  * ---------------------------------------------------------------------------------------------
  */
+import { ErrorMessage } from "@quatico/websmith-api";
 import { compileSystem } from "../../../test";
 import { NoReporter } from "../NoReporter";
 import { resolveCompilationConfig } from "./resolve-compiler-config";
@@ -34,10 +35,88 @@ describe("resolveCompilationConfig", () => {
     });
 
     it("should return defaults w/ existing path and empty config file", () => {
-        const { fileSystem: target } = compileSystem({ "./empty-config.json": "{}" });
+        const { fileSystem: target } = compileSystem({ "./empty-config.json": JSON.stringify({}) });
 
         const actual = resolveCompilationConfig("./empty-config.json", new NoReporter(), target);
 
         expect(actual).toEqual({});
+    });
+
+    it("should return config properties w/ existing path and full config", () => {
+        const { fileSystem: target } = compileSystem({
+            "./target-config.json": JSON.stringify({
+                addons: ["addon1", "addon2"],
+                addonsDir: "./addons",
+                profiles: {
+                    profile1: {
+                        addons: ["other-addon"],
+                        tsConfig: {
+                            outDir: "./dist",
+                        },
+                    },
+                },
+                transpileOnly: true,
+            }),
+        });
+
+        const actual = resolveCompilationConfig("./target-config.json", new NoReporter(), target);
+
+        expect(actual).toEqual({
+            addons: ["addon1", "addon2"],
+            addonsDir: "/addons",
+            profiles: {
+                profile1: {
+                    addons: ["other-addon", "addon1", "addon2"],
+                    tsConfig: {
+                        outDir: "/dist",
+                    },
+                },
+            },
+            transpileOnly: true,
+        });
+    });
+
+    it("should throw error w/ non-existing profile dependencies", () => {
+        const targetFn = jest.spyOn(NoReporter.prototype, "reportDiagnostic");
+
+        const { fileSystem } = compileSystem({
+            "./target-config.json": JSON.stringify({
+                profiles: {
+                    profile1: {
+                        depends: ["unknown-profile"],
+                    },
+                },
+            }),
+        });
+
+        resolveCompilationConfig("./target-config.json", new NoReporter(), fileSystem);
+
+        expect(targetFn).toHaveBeenCalledWith(new ErrorMessage("Unknown profile 'unknown-profile' in 'depends' of './target-config.json'."));
+    });
+
+    it("should throw error w/ existing and non-existing profile dependencies", () => {
+        const targetFn = jest.spyOn(NoReporter.prototype, "reportDiagnostic");
+
+        const { fileSystem } = compileSystem({
+            "./target-config.json": JSON.stringify({
+                profiles: {
+                    profile1: {
+                        depends: ["unknown-profile", "profile2"],
+                        tsConfig: {
+                            outDir: "./dist",
+                        },
+                    },
+                    profile2: {
+                        tsConfig: {
+                            outDir: "./dist",
+                        },
+                    },
+                },
+            }),
+        });
+
+        resolveCompilationConfig("./target-config.json", new NoReporter(), fileSystem);
+
+        expect(targetFn).toHaveBeenCalledWith(new ErrorMessage("Unknown profile 'unknown-profile' in 'depends' of './target-config.json'."));
     });
 });
