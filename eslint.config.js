b213d591f3d355a3ccdd417fe3967ef94d60b1bc
Jan Wloka <jan.wloka@quatico.com>
2025-03-16 21:34:48 +0100

    e Improves project and jest setup
    + Remove .js from testRegex and don't watch generated files
    + Remove deprecated @types declarations
    + Fixes linting issues in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 /*
  * ---------------------------------------------------------------------------------------------
  *   Copyright (c) Quatico Solutions AG. All rights reserved.
