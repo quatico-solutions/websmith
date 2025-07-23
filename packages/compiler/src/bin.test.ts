0bfa4ded463cd024e667b04ab42c2f8c0a6b9768
Jan Wloka <jan.wloka@quatico.com>
2025-07-23 22:56:06 +0200

    a Adds missing license headers and ignore test-data files from license checks

--- packages/compiler/src/bin.test.ts
+++ packages/compiler/src/bin.test.ts
@@ -1,3 +1,9 @@
+/*
+ * ---------------------------------------------------------------------------------------------
+ *   Copyright (c) Quatico Solutions AG. All rights reserved.
+ *   Licensed under the MIT License. See LICENSE in the project root for license information.
+ * ---------------------------------------------------------------------------------------------
+ */
 import path from "node:path";
 import fs from "node:fs";
 import { execSync } from "node:child_process";
