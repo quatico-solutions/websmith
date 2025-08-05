e5b400cf4697a4524fb68dd762935470ce168706
Jan Wloka <jan.wloka@quatico.com>
2025-08-05 07:54:27 +0200

    t Ensure that compiler tests run not in parallel

--- packages/compiler/jest.config.ts
+++ packages/compiler/jest.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
         "@quatico/websmith-core": "<rootDir>/../core/src",
         "@quatico/websmith-testing": "<rootDir>/../testing/src",
     },
+    maxWorkers: 1, // runInBand: true is not supported as config option
 };
 
 export default config;
