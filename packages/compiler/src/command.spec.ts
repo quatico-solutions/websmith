c8613415bfb02f3a51f50f350f4c3447480bfa6d
Jan Wloka <jan.wloka@quatico.com>
2025-08-13 15:51:37 +0200

    a Linting fixes
    #91

--- packages/compiler/src/command.spec.ts
+++ packages/compiler/src/command.spec.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access */
 /*
  * ---------------------------------------------------------------------------------------------
  *   Copyright (c) Quatico Solutions AG. All rights reserved.
